dbcb5ef47af200749152e28f79d8d30a5d6d95e8
LadyNerevar <Ladynerevar@gmail.com>
2024-12-05 01:13:16 -0800

    open sans condensed

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Roboto_Mono as FontMono, Jost as FontSans } from "next/font/google"
+import { Roboto_Mono as FontMono, Open_Sans_Condensed as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
