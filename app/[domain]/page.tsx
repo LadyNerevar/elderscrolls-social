c0a2ca2b64c6b07be15081b44873dd3f57824873
LadyNerevar <Ladynerevar@gmail.com>
2024-12-05 00:39:43 -0800

    don't break the build

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -197,9 +197,15 @@ export default async function IndexPage({
           disabled={!newHandle || !!error2}
           last
         >
-        <Check className="size-4 text-green-500" /> Success! New name created.
-        </p>
-        <p className="max-w-lg text-sm">
+              {!!error2  (
+                <>
+                  <p className="text-muted-forground mt-4 flex flex-row items-center gap-2 text-sm">
+                    <Check className="size-4 text-green-500" /> Account found
+                  </p>
+                  <Profile profile={profile} className="mt-4" />
+                </>
+              )}
+        <p className="max-w-lg text-sm text-muted-foreground">
             Go to Settings {">"} Advanced {">"} Change my handle. Select &quot;I
             have my own domain&quot; and enter{" "}
             {newHandle ? `"${newHandle}"` : "your new handle"}. Finally, tap
