41fd7a0ac10c706d0c935b8a83c4f7d4ddc50227
LadyNerevar <99475825+LadyNerevar@users.noreply.github.com>
2024-12-05 11:28:03 -0800

    added message about losing your old handle

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -203,10 +203,13 @@ export default async function IndexPage({
             {newHandle ? `"${newHandle}"` : "your new handle"}. Finally, tap
             &quot;Verify DNS Record&quot;.
           </p>
+
+          <p>Note: if you change your default Bluesky username (with the .bsky.social suffix) to a website URL, your old username will be available for someone else to use. However, any tags or mentions with your old handle will still point to your account. If you'd like to keep your old .bsky.social username, we recommend creating a second account to hold that username.</p>
+          
           <p className="mt-6 max-w-lg text-sm">
             This page is forked from @samuel.bsky.team. If you like this project, consider{" "}
             <a href="https://github.com/sponsors/mozzius" className="underline">
-              sponsoring his work
+              sponsoring Sam's work
             </a>
             .
           </p>
