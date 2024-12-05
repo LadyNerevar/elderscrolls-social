2cfa8750e0a9f4efd7488396be97ffd57ae379f7
LadyNerevar <Ladynerevar@gmail.com>
2024-12-05 01:24:28 -0800

    pt san narrow

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Roboto_Mono as FontMono, Open_Sans_Condensed as FontSans } from "next/font/google"
+import { Roboto_Mono as FontMono, PT_Sans_Narrow as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
